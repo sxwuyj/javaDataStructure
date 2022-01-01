6b6b5dced76798d9ca670b88053a3fff65dd1a4b
wuyanjun <wuyanjun@xiaolian.link>
2022-01-01 23:36:03 +0800

    day16

---
+++ src/com/wuyj/algorithm/day16/Test03.java
@@ -0,0 +1,42 @@
+package com.wuyj.algorithm.day16;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wuyj
+ * @date 2022/1/1 11:32 下午
+ *
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ */
+public class Test03 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> path_p = getPath(root, p);
+        List<TreeNode> path_q = getPath(root, q);
+        TreeNode ancestor = null;
+        for (int i = 0; i < path_p.size() && i < path_q.size(); ++i) {
+            if (path_p.get(i) == path_q.get(i)) {
+                ancestor = path_p.get(i);
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+
+    public List<TreeNode> getPath(TreeNode root, TreeNode target) {
+        List<TreeNode> path = new ArrayList<TreeNode>();
+        TreeNode node = root;
+        while (node != target) {
+            path.add(node);
+            if (target.val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        path.add(node);
+        return path;
+    }
+}
